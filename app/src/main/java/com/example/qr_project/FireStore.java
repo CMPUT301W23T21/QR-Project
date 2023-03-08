fbbb75a8367337df3d2550880ce1390243465689
khoile99999 <phuocngu@ualberta.ca>
2023-03-07 21:43:09 -0700

    Demo, please dont merge yet

--- app/src/main/java/com/example/qr_project/FireStore.java
+++ app/src/main/java/com/example/qr_project/FireStore.java
@@ -2,6 +2,8 @@ package com.example.qr_project;
 
 /*
    FireStore can store the QR Codes in the collection online and remotely.
+   Getting the data from the user and then uploading the date online right away to the collection
+   Every QR Code will be stored online on Cloud FireBase
  */
 
 
