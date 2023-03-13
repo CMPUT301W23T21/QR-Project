0ef30850aab042cd9847f1aa9d1b502e9cb76d7f
PashokKalashnikov <pashok.kalashnikov228@gmail.com>
2023-03-13 14:53:27 -0600

    Delete Click added

--- app/src/main/java/com/example/qr_project/activities/QRCodeActivity.java
+++ app/src/main/java/com/example/qr_project/activities/QRCodeActivity.java
@@ -1,6 +1,8 @@
 package com.example.qr_project.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +14,20 @@ public class QRCodeActivity extends AppCompatActivity {
      * After that, it will move on the next step/page
      * @param savedInstanceState   a package to be called to execute the QRCodeActivity
      */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+    }
+
+    /**
+     * For the use and the feature of Delete click button
+     *
+     * @param view The text view which is pressed
+     */
+    public void onDeleteClick(View view) {
+        Toast.makeText(this, "Delete Button Click", Toast.LENGTH_SHORT).show();
     }
 }
