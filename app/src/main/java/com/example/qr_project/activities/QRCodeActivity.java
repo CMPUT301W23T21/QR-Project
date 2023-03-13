8d2866c255b60b566c8fa8a48f78d54949ec9c95
PashokKalashnikov <pashok.kalashnikov228@gmail.com>
2023-03-13 15:23:40 -0600

    DeleteClick added

--- app/src/main/java/com/example/qr_project/activities/QRCodeActivity.java
+++ app/src/main/java/com/example/qr_project/activities/QRCodeActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.qr_project.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class QRCodeActivity extends AppCompatActivity {
     /**
@@ -15,11 +17,16 @@ public class QRCodeActivity extends AppCompatActivity {
      * @param savedInstanceState   a package to be called to execute the QRCodeActivity
      */
 
+    private FirebaseFirestore db;
+    private CollectionReference collectionReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        db = FirebaseFirestore.getInstance();
+        collectionReference = db.collection("users");
     }
 
     /**
@@ -29,5 +36,11 @@ public class QRCodeActivity extends AppCompatActivity {
      */
     public void onDeleteClick(View view) {
         Toast.makeText(this, "Delete Button Click", Toast.LENGTH_SHORT).show();
+        String userID = "dfgg";
+        collectionReference
+                .document(userID)
+                        .get("qrcodes")
+        cityList.setItemChecked(-1, true);
+        selectedIndex = -1;
     }
 }
